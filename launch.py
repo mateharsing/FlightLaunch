3ba0dd3cb65acc4203bb2bfd5d0320f74fabc654
mateharsing <mharsing@jamasoftware.com>
2024-09-26 09:26:33 +0200

    Update launch.py AIRS2-LLR-2

--- launch.py
+++ launch.py
@@ -3,7 +3,7 @@ import time
 class Rocket:
     def __init__(self, name):
         self.name = name
-        self.fuel_level = 10005
+        self.fuel_level = 10006
         self.altitude = 0
         self.velocity = 0
         self.launched = False
